d8789379d40b8ca6aa294e02feac31cb64cc49fd
njayamangala <135201131+njayamangala@users.noreply.github.com>
2024-03-01 13:07:13 +0530

    array and string practice

---
+++ 02-get-sum/get-sum.js
@@ -0,0 +1,3 @@
+function getSum(num1,num2) {return num1+num2}
+
+module.exports = getSum;
---
+++ 03-calculator/calculator.js
@@ -0,0 +1,5 @@
+function calculator(num1,num2,op) {
+    return eval(num1 + op+ num2)
+}
+
+module.exports = calculator;
